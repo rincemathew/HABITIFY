da26467cb5814fc522cc65798d1731105e3c9044
Rince Mathew <rincemathewmanattu@gmail.com>
2025-12-13 17:53:54 +0530

    tab navigation

--- app/(tabs)/focus.tsx
+++ app/(tabs)/focus.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function index() {
+export default function focus() {
     return (
         <View>
-            <Text>index</Text>
+            <Text>focus</Text>
         </View>
     )
 }
