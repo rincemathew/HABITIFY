6c9c834bc7e812aa1ccc34abdadd9a278c35848d
Rince Mathew <rincemathewmanattu@gmail.com>
2025-12-15 19:21:08 +0530

    fixed drawer and tab in new way

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,8 +1,12 @@
+import { DrawerToggleButton } from "@react-navigation/drawer";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs screenOptions={{
+            headerLeft: () =>
+                <DrawerToggleButton />
+        }}>
             <Tabs.Screen name="index" options={{ title: "Home", }} />
             <Tabs.Screen name="focus" options={{ title: "Focus", }} />
             {/* <Tabs.Screen name="focus" options={{ title: "Focus", tabBarBadge: 3 }} /> */}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,12 @@
-// import { Stack } from "expo-router";
-import { useSegments } from "expo-router";
-import { Drawer } from "expo-router/drawer";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-
-type TabKey = "index" | "focus";
-
-const TAB_TITLES: Record<TabKey, string> = {
-  index: "Home",
-  focus: "Focus",
-  // add: "Add",
-};
+import { Stack } from "expo-router";
+import React from 'react';
+import { StyleSheet } from 'react-native';
 
 export default function RootLayout() {
-
-  const segments = useSegments();
-
-  const activeTab: TabKey =
-    (segments.find((seg) => seg in TAB_TITLES) as TabKey) ?? "index";
-
-  const title = TAB_TITLES[activeTab];
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer screenOptions={{ headerTitle: title }}>
-        <Drawer.Screen name="(tabs)" options={{ title: "Home" }} />
-      </Drawer>
-    </GestureHandlerRootView>
+    <Stack screenOptions={{ headerShown: false }}>
+    </Stack>
   )
 }
+
+const styles = StyleSheet.create({})
